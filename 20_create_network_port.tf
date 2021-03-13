247a51968a51c73e4483f11dc74bc37401a46fbf
Johannes Kastl <kastl@b1-systems.de>
2021-03-13 22:36:44 +0100

    ADD 20_create_network_port.tf

---
+++ 20_create_network_port.tf
@@ -0,0 +1,17 @@
+# create ports to use with the instances
+
+resource "openstack_networking_port_v2" "instance_ports" {
+  count              = var.desired_number
+  name               = "${var.port_name_or_prefix}${format("%02d", count.index + 1)}"
+  network_id         = var.port_network_id
+  admin_state_up     = var.port_admin_state_up
+  security_group_ids = var.port_security_group_ids
+  depends_on         = [var.module_depends_on]
+
+  dynamic "allowed_address_pairs" {
+    for_each = var.port_allowed_ip_adresses == [] ? [] : var.port_allowed_ip_adresses
+    content {
+      ip_address = allowed_address_pairs.value[0]
+    }
+  }
+}
